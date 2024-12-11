b31b82085d379c3eee4a1e25d7574213f12a44ca
Wagner Maciel <wagner.varginha@hotmail.com>
2024-12-10 22:39:06 -0300

    build(react): update react to version 19

--- src/components/App.spec.tsx
+++ src/components/App.spec.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import {render} from "@testing-library/react";
 
-import App from "../components/App";
+import App from "./App";
 
 
 test("Must pass", async function () {
